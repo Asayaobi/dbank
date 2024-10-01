09222b613123e7ff0297ef477e05493802188176
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-01 18:55:02 +0200

    Update index.js

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -8,37 +8,15 @@ window.addEventListener("load", async function() {
   document.getElementById("value").innerText = roundAmount;
 });
 
-// document.querySelector("form").addEventListener("submit", async function(event) {
-//   event.preventDefault();
-//   // console.log("Submitted.");
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  // console.log("Submitted.");
 
-//   const button = event.target.querySelector("#submit-btn");
+  //topUp functiom
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    console.log('topup amount:', inputAmount, typeof inputAmount);
 
-//   const inputAmount = parseFloat(document.getElementById("input-amount").value);
-//   const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+    await dbank.topUp(inputAmount);
+    console.log('topup done');
+});
 
-//   button.setAttribute("disabled", true);
-
-//   if (document.getElementById("input-amount").value.length != 0) {
-//     await dbank.topUp(inputAmount);
-//   }
-
-//   if (document.getElementById("withdrawal-amount").value.length != 0) {
-//     await dbank.withdraw(outputAmount);
-//   }
-
-//   await dbank.compound();
-
-//   update()
-
-//   document.getElementById("input-amount").value = "";
-//   document.getElementById("withdrawal-amount").value = "";
-
-//   button.removeAttribute("disabled");
-
-// });
-
-// async function update() {
-//   const currentAmount = await dbank.checkBalance();
-//   document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
-// };
