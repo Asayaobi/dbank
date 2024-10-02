f2ea2d6d8ede127b55fcbc9e409addc282ca3f9b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-02 10:20:05 +0200

    add updateBalance function

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -21,8 +21,17 @@ document.querySelector("form").addEventListener("submit", async function(event) 
     //while calling the function, makes the button disable
     button.setAttribute("disabled",true);
     await dbank.topUp(inputAmount);
-    //when the function is done, remove disabled button
+    //when the function is done, 
+    //update the balance
+    await updateBalance()
+    //remove disabled button
     console.log('topup done');
     button.removeAttribute("disabled");
 });
 
+async function updateBalance() {
+  const currentAmount = await dbank.checkBalance();
+  // round the amount to 2 digits
+  const roundAmount = Math.round(currentAmount * 100) / 100
+  document.getElementById("value").innerText = roundAmount;
+};
